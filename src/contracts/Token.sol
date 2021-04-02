3206765f32433d86b36b346dd4cec6a1bf9bed1b
TeddReyn <23265523+EliasOne@users.noreply.github.com>
2021-04-02 01:40:56 -0400

    fix solidity -v

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,29 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  address public minter; //add minter variable
+  address public minter;
 
-  //add minter changed event
   event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("TestToken", "DFI") {
-    minter = msg.sender //assign initial minter
+  constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
+    minter = msg.sender; //only initially
   }
+
   function passMinterRole(address dBank) public returns (bool) {
-    require(msg.sender==minter, 'Error, only owner can change minter role');
-    minter = dBank;
+  	require(msg.sender==minter, 'Error, only owner can change pass minter role');
+  	minter = dBank;
 
     emit MinterChanged(msg.sender, dBank);
     return true;
   }
-  //Add pass minter role function
 
   function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-    require(msg.sender ==minter, 'Error, msg.sender does not have minter role')
-		_mint(account, amount);
+		require(msg.sender==minter, 'Error, msg.sender does not have minter role'); //dBank
+	  _mint(account, amount);
 	}
 }
